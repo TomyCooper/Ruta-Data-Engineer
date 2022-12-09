6dd436e2dfdb423d91b1ce63498a9ece5033c879
TomyCooper <t.robles10@uniandes.edu.co>
2022-12-09 13:06:25 -0500

    se realizaron múltiple cambios en los archivos dentro de la carpeta app para generar un proyecto de gráfica de la población de un país dado por parámetro.

--- Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/main.py
+++ Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/main.py
@@ -1,24 +1,19 @@
 import utils
-
-data = [
-    {
-        'Country': 'Colombia',
-        'Population': 500
-    },
-    {
-        'Country': 'Bolivia',
-        'Population': 300
-    }
-]
+import read_csv
+import charts
 
 def run():
-    keys,values = utils.get_population()
-    print(keys, values)
+    data = read_csv.read_csv('/Users/tomycooper/Documents/Ruta Data Engineer/Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/data.csv')
 
     country = input('Ingresa el país:')
     
-    result = utils.population_by_country(data, country)
-    print(result)
+    country_dict = utils.population_by_country(data, country)
+
+    if len(country_dict) > 0:
+        country = country_dict[0]
+        labels,values = utils.get_population(country)
+
+        charts.generate_bar_chart(labels, values)
 
 if __name__ == '__main__':
-    run
+    run()
