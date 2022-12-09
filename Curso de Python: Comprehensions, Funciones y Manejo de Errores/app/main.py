3d2754a64c35c5f6c4ca321071933fbf4c73cc43
TomyCooper <t.robles10@uniandes.edu.co>
2022-12-09 13:19:12 -0500

    Terminé el proyecto de graficar el porcentaje de población por país en un continente dado por parámetro.

--- Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/main.py
+++ Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/main.py
@@ -5,6 +5,16 @@ import charts
 def run():
     data = read_csv.read_csv('/Users/tomycooper/Documents/Ruta Data Engineer/Curso de Python: Comprehensions, Funciones y Manejo de Errores/app/data.csv')
 
+    continent = input('Ingresa el continente que deseas consultar:')
+
+    data = list(filter(lambda x: x['Continent'] == continent, data))
+
+    countries = list(map(lambda x: x['Country/Territory'], data))
+
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+
+    charts.generate_pie_chart(countries, percentages)
+    """
     country = input('Ingresa el país:')
     
     country_dict = utils.population_by_country(data, country)
@@ -14,6 +24,6 @@ def run():
         labels,values = utils.get_population(country)
 
         charts.generate_bar_chart(labels, values)
-
+    """
 if __name__ == '__main__':
     run()
