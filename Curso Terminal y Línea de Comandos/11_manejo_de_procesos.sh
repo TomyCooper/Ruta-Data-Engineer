56bdc307ec9fe70cbd150ee474fd0e2c45c74a07
TomyCooper <t.robles10@uniandes.edu.co>
2022-12-10 10:11:51 -0500

    Aprendí de administración de procesos.

---
+++ Curso Terminal y Línea de Comandos/11_manejo_de_procesos.sh
@@ -0,0 +1,10 @@
+# Para ver los procesos actuales
+ps
+# Nota: puedes acordarte de esto en Docker
+
+# Para matar un proceso
+kill id
+    id  :id del proceso
+
+# Un comando para ver TODOS los procesos
+top
