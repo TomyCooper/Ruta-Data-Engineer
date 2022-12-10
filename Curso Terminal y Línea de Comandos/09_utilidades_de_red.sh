d25965e07bef64d47d06a363b23a51efbfb0e379
TomyCooper <t.robles10@uniandes.edu.co>
2022-12-10 09:46:11 -0500

    Aprendí algunos comandos para generar utilidades de red.

---
+++ Curso Terminal y Línea de Comandos/09_utilidades_de_red.sh
@@ -0,0 +1,15 @@
+# Quiero monitorear una página.
+ping google.com
+
+# Quiero leer el archivo html de una página
+curl google.com
+
+# Quiero descargar dicho html
+wget google.com
+
+# Quiero ver todas las máquinas que intervienen en el manejo de una página.
+traceroute google.com
+
+# mostrar dispositivos de red.
+netstat
+ifconfig
